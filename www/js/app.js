74f32bb517e8d2c80bb340eb732b3b76f74362b1
Jhony Lucas <jhonyjl37@gmail.com>
2014-12-05 11:16:37 -0300

    primeiro commit

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.controller('novoTicketController', function($window) {
+  this.setor = '';
+
+  this.toggle = function(){
+    $window.alert(greeting);
+    this.setor = "sd";
+
+  };
+
+});
